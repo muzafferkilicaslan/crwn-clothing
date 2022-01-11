4cf059ba963a488fed21d1323d5fa0b3a2e77604
thepitohui <thepitohui@gmail.com>
2022-01-11 12:49:24 +0300

    added cartIcon and cartDropDown components to our application, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import { Routes, Route, Link, Redirect, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
