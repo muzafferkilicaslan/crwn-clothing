d853ad885efce0b559fe7b7e2e033c268bc48a3c
MUZAFFER KILIÇASLAN <2016010205058@ogrenci.karabuk.edu.tr>
2022-01-30 14:12:12 +0300

    updated history.push()

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,7 @@ import {withRouter} from "react-router-dom";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
   <div className={`${size} menu-item`}
-  onClick={() => history.push(`${match.url}${linkUrl}`)}>
+  onClick={() => history.push(`${linkUrl}`)}>
     <div
       className='background-image'
       style={{
